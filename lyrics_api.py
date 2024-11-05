339615324852f3d9253048877981ae534c3c84e6
Hutch Cummings <JMack89427@gmail.com>
2024-11-05 16:44:28 +0000

    More labs

---
+++ lyrics_api.py
@@ -0,0 +1,21 @@
+import requests
+
+def main():
+    # Get user input for band and song (with defaults)
+    band = input("Enter the band name (default: 'Beatles'): ") or "Beatles"
+    song = input("Enter the song title (default: 'Let it Be'): ") or "Let it Be"
+
+    # Construct the URL
+    url = f"https://api.lyrics.ovh/v1/{band}/{song}".replace(" ", "%20")
+    print(url)
+
+    # Send GET request
+    response = requests.get(url)
+    lyrics = response.json()
+    if response.ok:
+        print(lyrics["lyrics"])
+    else:
+        print("Lyrics not found")
+
+if __name__ == "__main__":
+    main()
