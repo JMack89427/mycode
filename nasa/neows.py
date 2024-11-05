787757fbbd610b647d30e4cba53271c5ec8e4ffb
Hutch Cummings <JMack89427@gmail.com>
2024-11-05 22:34:54 +0000

    Updated code

--- nasa/neows.py
+++ nasa/neows.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import requests
+from datetime import datetime
+import pprint
 
 ## Define NEOW URL
 NEOURL = "https://api.nasa.gov/neo/rest/v1/feed?"
 
 # this function grabs our credentials
-# it is easily recycled from our previous script
 def returncreds():
     ## first I want to grab my credentials
     with open("/home/student/nasa.creds", "r") as mycreds:
@@ -14,26 +15,46 @@ def returncreds():
     nasacreds = "api_key=" + nasacreds.strip("\n")
     return nasacreds
 
+# this function validates the date format
+def validate_date(date_text):
+    try:
+        datetime.strptime(date_text, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
+
 # this is our main function
 def main():
     ## first grab credentials
     nasacreds = returncreds()
 
-    ## update the date below, if you like
-    startdate = "start_date=2019-11-11"
+    ## prompt the user for a start date and validate it
+    while True:
+        user_start_date = input("Enter the start date (YYYY-MM-DD): ")
+        if validate_date(user_start_date):
+            break
+        else:
+            print("Invalid date format. Please enter the date in YYYY-MM-DD format.")
+    startdate = f"start_date={user_start_date}"
 
-    ## the value below is not being used in this
-    ## version of the script
-    # enddate = "end_date=END_DATE"
+    ## prompt the user for an optional end date and validate it if provided
+    user_end_date = input("Enter the end date (YYYY-MM-DD) or press Enter to skip: ")
+    if user_end_date:
+        while not validate_date(user_end_date):
+            print("Invalid date format. Please enter the date in YYYY-MM-DD format.")
+            user_end_date = input("Enter the end date (YYYY-MM-DD) or press Enter to skip: ")
+        enddate = f"&end_date={user_end_date}"
+    else:
+        enddate = ""  # no end date parameter if skipped
 
-    # make a request with the request library
-    neowrequest = requests.get(NEOURL + startdate + "&" + nasacreds)
+    ## make a request with the request library
+    neowrequest = requests.get(NEOURL + startdate + enddate + "&" + nasacreds)
 
     # strip off json attachment from our response
     neodata = neowrequest.json()
 
     ## display NASAs NEOW data
-    print(neodata)
+    pprint.pprint(neodata)
 
 if __name__ == "__main__":
     main()
